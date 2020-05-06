26bcf0b0f1d68923075e82485cb7cd1b92a47dc0
Roy Chiang <ann1234555@yahoo.com.tw>
2020-05-06 15:54:36 +0800

    init

---
+++ app/controllers/ghost_controller.rb
@@ -0,0 +1,6 @@
+class GhostController < ApplicationController
+	def eat
+		render plain: "吃土"
+	end
+
+end
---
+++ test/controllers/ghost_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class GhostControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
